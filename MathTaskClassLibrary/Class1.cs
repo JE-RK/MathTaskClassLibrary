7848a9511cd345edbb461b192013e02ee994e310
Dima <belomytsev78@gmail.com>
2023-03-30 16:21:30 +0300

    Добавьте файлы проекта.

---
+++ MathTaskClassLibrary/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathTaskClassLibrary
+{
+    public class Geometry
+    {
+        double h;
+        double r;
+        public double CylinderVolume(double h, double r)
+        {
+            double result = Math.PI * Math.Pow(r, 2) * h;
+            return result;
+        }
+    }
+}
---
+++ MathTaskClassLibraryTests/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MathTaskClassLibrary;
+
+namespace MathTaskClassLibraryTests
+{
+    [TestClass]
+    public class GeometryTest
+    {
+        [TestMethod]
+        public void CylinderVolume_12and50()
+        {
+            double r = 12;
+            double h = 50;
+            double expected = 22619.46710584651;
+            Geometry g = new Geometry();
+            Assert.IsTrue(Math.Abs(expected - g.CylinderVolume(h, r)) < 1e-9);
+        }
+        [TestMethod]
+        public void CylinderVolume1()
+        {
+            double r = -12;
+            double h = 50;
+            double expected = 22619.46710584651;
+            Geometry g = new Geometry();
+            Assert.IsTrue(Math.Abs(expected - g.CylinderVolume(h, r)) < 1e-9);
+        }
+        [TestMethod]
+        public void CylinderVolume2()
+        {
+            double r = 0;
+            double h = 25;
+            double expected = 22619.46710584651;
+            Geometry g = new Geometry();
+            Assert.IsTrue(Math.Abs(expected - g.CylinderVolume(h, r)) < 1e-9);
+        }
+        [TestMethod]
+        public void CylinderVolume3()
+        {
+            double r = 56;
+            double h = 0;
+            double expected = 0;
+            Geometry g = new Geometry();
+            Assert.IsTrue(Math.Abs(expected - g.CylinderVolume(h, r)) < 1e-9);
+        }
+        [TestMethod]
+        public void CylinderVolume4()
+        {
+            double r = 56;
+            double h = 100;
+            double expected = 5185.8304;
+            Geometry g = new Geometry();
+            Assert.IsTrue(Math.Abs(expected - g.CylinderVolume(h, r)) < 1e-9);
+        }
+        [TestMethod]
+        public void CylinderVolume5()
+        {
+            double r = -56;
+            double h = -100;
+            double expected = 22619.46710584;
+            Geometry g = new Geometry();
+            Assert.IsTrue(Math.Abs(expected - g.CylinderVolume(h, r)) < 1e-9);
+        }
+    }
+}
